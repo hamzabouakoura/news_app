a6c0b0e1261ff2405345edf7b4f51777bac6dd03
hamzabouakoura <103844829+hamzabouakoura@users.noreply.github.com>
2024-03-26 14:40:54 +0100

    Update api_services.dart

--- lib/core/utils/api_services.dart
+++ lib/core/utils/api_services.dart
@@ -3,11 +3,11 @@ import 'package:dio/dio.dart';
 class ApiServices {
   ApiServices(this._dio);
   final Dio _dio;
-  final String apiKey = '54abf2813f7b4660b61dd12911495870';
+  final String _apiKey = API_KEY;
   final String _baseUrl = 'https://newsapi.org/v2/';
 
   Future<Map<String, dynamic>> get({required String endPoint}) async {
-    var response = await _dio.get('$_baseUrl$endPoint&apiKey=$apiKey');
+    var response = await _dio.get('$_baseUrl$endPoint&_apiKey=$_apiKey');
     return response.data;
   }
 }
